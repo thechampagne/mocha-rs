eca99a384323ed34db57ee3d873bc0beb33f4b55
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-10-21 22:55:38 +0300

    add Mocha::parse and Mocha::field_get

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod raw;
+use std::ffi::CStr;
 
 #[derive(Debug)]
 pub enum MochaError {
@@ -49,4 +50,61 @@ pub struct Object {
 #[derive(Debug)]
 pub struct Mocha {
     obj: raw::mocha_object_t,
+}
+
+impl Mocha {
+    pub fn parse(src: &str) -> Result<Self, MochaError> {
+	unsafe {
+	    let mut object = raw::mocha_object_t{fields: 0 as _, fields_len: 0};
+	    let mocha = raw::mocha_nparse(&mut object as _, src.as_ptr() as _, src.len());
+	    if let Some(err) = handle_mocha_error(mocha) {
+		Err(err)
+	    } else {
+		Ok(Self {obj: object})
+	    }
+	}
+    }
+
+    pub fn field_get(&self, index: usize) -> Option<Field> {
+	unsafe {
+	    if index >= self.obj.fields_len { return None }
+	    let field = raw::mocha_field(&self.obj as _, index);
+	    match field.type_ {
+		raw::mocha_value_type_t_MOCHA_VALUE_TYPE_NIL => Some(Field { name: CStr::from_ptr(field.name).to_bytes(),
+									     value: Value::Nil }),
+		raw::mocha_value_type_t_MOCHA_VALUE_TYPE_STRING => Some(Field { name: CStr::from_ptr(field.name).to_bytes(),
+										value: Value::String(CStr::from_ptr(field.value.string).to_bytes()) }),
+		raw::mocha_value_type_t_MOCHA_VALUE_TYPE_REFERENCE => Some(Field { name: CStr::from_ptr(field.name).to_bytes(),
+										   value: Value::Ref(Reference{reference: field.value.reference}) }),
+		raw::mocha_value_type_t_MOCHA_VALUE_TYPE_BOOLEAN => Some(Field { name: CStr::from_ptr(field.name).to_bytes(),
+										 value: Value::Bool(if field.value.boolean == 0 { false } else { true }) }),
+		raw::mocha_value_type_t_MOCHA_VALUE_TYPE_OBJECT => Some(Field { name: CStr::from_ptr(field.name).to_bytes(),
+										value: Value::Object(Object{obj: field.value.object}) }),
+		raw::mocha_value_type_t_MOCHA_VALUE_TYPE_ARRAY => Some(Field { name: CStr::from_ptr(field.name).to_bytes(),
+									       value: Value::Array(Array{array: field.value.array}) }),
+		raw::mocha_value_type_t_MOCHA_VALUE_TYPE_FLOAT64 => Some(Field { name: CStr::from_ptr(field.name).to_bytes(),
+										 value: Value::Float(field.value.float64) }),
+		raw::mocha_value_type_t_MOCHA_VALUE_TYPE_INTEGER64 => Some(Field { name: CStr::from_ptr(field.name).to_bytes(),
+										 value: Value::Int(field.value.integer64) }),
+		_ => None
+	    }
+	}
+    }
+}
+
+#[inline(always)]
+fn handle_mocha_error(err: raw::mocha_error_t) -> Option<MochaError> {
+    match err {
+	raw::mocha_error_t_MOCHA_ERROR_NONE => None,
+	raw::mocha_error_t_MOCHA_ERROR_MISSING_FIELD => Some(MochaError::MissingField),
+	raw::mocha_error_t_MOCHA_ERROR_DUPLICATE_FIELD => Some(MochaError::DuplicateField),
+	raw::mocha_error_t_MOCHA_ERROR_ROOT_REFERENCE => Some(MochaError::RootReference),
+	raw::mocha_error_t_MOCHA_ERROR_OUT_OF_MEMORY => Some(MochaError::OutOfMemory),
+	raw::mocha_error_t_MOCHA_ERROR_INVALID_CHARACTER => Some(MochaError::InvalidCharacter),
+	raw::mocha_error_t_MOCHA_ERROR_OVERFLOW => Some(MochaError::Overflow),
+	raw::mocha_error_t_MOCHA_ERROR_END_OF_STREAM => Some(MochaError::EndOfStream),
+	raw::mocha_error_t_MOCHA_ERROR_UNEXPECTED_TOKEN => Some(MochaError::UnexpectedToken),
+	raw::mocha_error_t_MOCHA_ERROR_UNEXPECTED_CHARACTER => Some(MochaError::UnexpectedCharacter),
+	_ => None,
+    }
 }
